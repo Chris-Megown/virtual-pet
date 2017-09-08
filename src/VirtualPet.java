62eb8a12192dd172ba69406f2cbe92c15e5a0482
Christopher A Megown <megown1204@gmail.com>
2017-09-07 20:29:49 -0400

    Hazza 2.0

--- src/VirtualPet.java
+++ src/VirtualPet.java
@@ -1,23 +1,60 @@
 
 public class VirtualPet {
 
-	public static String menuPet() {
-		return "Press 1 to Feed your pet.\nPress 2 to Water your pet.\nPress 3 to let your pet Rest.\nPress 4 to Play with you pet.\nPress 5 to do nothing.\nPlease respond: ";
-	}
-
+	String petName;
 	int hunger = 50;
 	int thirst = 50;
 	int tiredness = 50;
+	int boredom = 50;
 
-	void feed(int amount) {
-		hunger += amount;
+	public static String menuPet() {
+		return "\nPress 1 to Feed your pet.\nPress 2 to Water your pet.\nPress 3 to let your pet Rest.\nPress 4 to Play with you pet.\nPress 5 to do nothing.\nPlease respond: ";
 	}
 
-	void water(int amount) {
-		thirst += amount;
+	public String statusPet() {
+		return petName + "'s current stats are as follows...\nHunger: " + hunger + "\nThirst: " + thirst
+				+ "\nTiredness: " + tiredness + "\nBoredom: " + boredom;
 	}
 
-	void rest(int amount) {
-		tiredness += amount;
+	boolean PetIsNotDed() {
+		if (hunger > 0 && thirst > 0 && tiredness > 0) {
+			return true;
+		}
+		return false;
+	}
+
+	void feed() {
+		hunger += 5;
+		thirst += 1;
+		tiredness -= 1;
+		boredom -= 2;
+	}
+
+	void water() {
+		hunger += 5;
+		thirst += 1;
+		tiredness -= 1;
+		boredom -= 2;
+	}
+
+	void rest() {
+		hunger -= 3;
+		thirst -= 3;
+		tiredness += 7;
+		boredom += 2;
+	}
+
+	void boredom() {
+		hunger -= 1;
+		thirst -= 1;
+		tiredness -= 1;
+		boredom += 6;
+	}
+
+	void tick() {
+		hunger -= 5;
+		thirst -= 5;
+		tiredness -= 1;
+		boredom -= 2;
 	}
 }
--- src/VirtualPetApp.java
+++ src/VirtualPetApp.java
@@ -7,11 +7,13 @@ public class VirtualPetApp {
 		Scanner input = new Scanner(System.in);
 		VirtualPet uberPet = new VirtualPet();
 		System.out.println("Welcome to Virtual Petworld 2.0");
+		System.out.print("Please name your pet: ");
+		uberPet.petName = input.next();
 
-		for (int tick = 0; tick < 10; tick++) {
-			String userChoice;
-
+		String userChoice;
+		while (uberPet.PetIsNotDed() == true) {
 			do {
+				System.out.println(uberPet.statusPet());
 				System.out.println(VirtualPet.menuPet());
 				userChoice = input.next();
 			} while (!userChoice.equals("1") && !userChoice.equals("2") && !userChoice.equals("3")
@@ -19,24 +21,33 @@ public class VirtualPetApp {
 
 			switch (userChoice) {
 			case "1":
-				System.out.println("\nYou feed your pet");
-				uberPet.feed(5);
+				System.out.println("\nYou feed " + uberPet.petName + " some food.");
+				uberPet.feed();
+				uberPet.tick();
 				System.out.println("Your pets Hunger is at: " + uberPet.hunger);
 				break;
 			case "2":
-				System.out.println("\nYou give you pet water");
-				uberPet.water(5);
+				System.out.println("\nYou give " + uberPet.petName + " some water.");
+				uberPet.water();
+				uberPet.tick();
 				System.out.println("Your pets Thirst is at: " + uberPet.thirst);
 				break;
 			case "3":
-				System.out.println("\nYour pets  is " +);
+				System.out.println("You lay " + uberPet.petName + " to rest for a bit.");
+				uberPet.rest();
+				uberPet.tick();
+				System.out.println("\nYour pets tiredness is " + uberPet.tiredness);
 				break;
 			case "4":
-				System.out.println("\nThank you, have a stupendous day!");
-				System.exit(0);
+				System.out.println("You play fetch with " + uberPet.petName + ".");
+				uberPet.boredom();
+				uberPet.tick();
+				System.out.println("\nYour pets boredom is: " + uberPet.boredom);
+				break;
 			}
 
 		}
+		input.close();
 	}
 
 }
